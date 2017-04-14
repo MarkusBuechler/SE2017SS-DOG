01b3e5fca4bdbaf8cb5983668c521b9ed69f26cb
Markus Buechler <markus.buechler@htwg-konstanz.de>
2017-04-14 13:33:33 +0200

    added field classes for subsequent implementation

---
+++ src/main/scala/de/htwg/se/yourgame/controller/fieldController.scala
@@ -0,0 +1,8 @@
+package de.htwg.se.yourgame.controller
+
+/**
+  * Created by margogo on 14.04.17.
+  */
+class fieldController {
+
+}
---
+++ src/main/scala/de/htwg/se/yourgame/model/Field.scala
@@ -0,0 +1,8 @@
+package de.htwg.se.yourgame.model
+
+/**
+  * Created by margogo on 14.04.17.
+  */
+case class Field (id: Integer, property: String, isUsed: Boolean, predecessor : Field, successor : Field) {
+
+}
