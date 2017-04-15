4da6bfbf62954c7b45071ecde2493be7cdbd3faf
Markus Buechler <markus.buechler@mail.de>
2017-04-15 10:02:06 +0200

    updated fields.csv to connect fields via successor and predeccessor

--- src/main/scala/de/htwg/se/yourgame/controller/fieldController.scala
+++ src/main/scala/de/htwg/se/yourgame/controller/fieldController.scala
@@ -19,19 +19,24 @@ class fieldController {
 
   var fieldList = new ListBuffer[Field]
 
-  val fieldForInit = Field(1,"String", "red", isUsed = false, null, null)
-  val fieldForInit2 = Field(2,"String", "red", isUsed = false, null, null)
+  val fieldForInit = Field(1,"String", "red", isUsed = false, Array(100), Array(100))
+  val fieldForInit2 = Field(2,"String", "red", isUsed = false, Array(100), Array(100))
 
   def initFields = {
     fieldList.clear
     val bufferedSource = io.Source.fromFile("resources/Fields.csv")
     for (line <- bufferedSource.getLines()) {
-      val Array(id, property, color, isUsed, predecessor, successor) = line.split(";").map(_.trim())
-      val bufferField = Field(id.toInt, property, color, isUsed.toBoolean, fieldForInit, fieldForInit2)
+      var Array(id, property, color, isUsed, predecessorIds, successorIds) = line.split(";").map(_.trim())
+
+      var intArraypredecessorId = predecessorIds.map(_.toInt)
+      var intArraySucessorId = successorIds.map(_.toInt)
+
+      val bufferField = Field(id.toInt, property, color, isUsed.toBoolean, intArraypredecessorId.toArray, intArraySucessorId.toArray)
       fieldList += bufferField
     }
 
     print("Fieldlist: " + fieldList.size + "\n");
+    print(fieldList)
   }
 
 }
--- src/main/scala/de/htwg/se/yourgame/model/Field.scala
+++ src/main/scala/de/htwg/se/yourgame/model/Field.scala
@@ -3,6 +3,6 @@ package de.htwg.se.yourgame.model
 /**
   * Created by margogo on 14.04.17.
   */
-case class Field (id: Integer, property: String, color: String, isUsed: Boolean , predecessor : Field, successor : Field) {
+case class Field (id: Integer, property: String, color: String, isUsed: Boolean , predecessorIds : Array[Int], successorIds : Array[Int]) {
 
 }
