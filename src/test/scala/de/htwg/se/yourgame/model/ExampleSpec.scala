2c50f9739d3347013275284923c31571eb0a543c
Markus Buechler <markus.buechler@mail.de>
2017-05-10 21:57:29 +0200

    added tests

---
+++ src/test/scala/de/htwg/se/yourgame/model/ExampleSpec.scala
@@ -0,0 +1,29 @@
+package de.htwg.se.yourgame.model
+
+import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest._
+import scala.collection.mutable
+
+/**
+  * Created by margogo on 10.05.17.
+  */
+class ExampleSpec extends FlatSpec with Matchers {
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new mutable.Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    stack.pop() should be(2)
+    stack.push(3)
+    stack.pop() should be(3)
+    stack.pop() should be(1)
+  }
+
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new mutable.Stack[Int]
+    a[NoSuchElementException] should be thrownBy {
+      emptyStack.pop()
+    }
+
+  }
+}
--- src/test/scala/de/htwg/se/yourgame/model/FieldSpec.scala
+++ src/test/scala/de/htwg/se/yourgame/model/FieldSpec.scala
@@ -2,21 +2,21 @@ package de.htwg.se.yourgame.model
 
 import org.scalatest._
 import org.junit.runner.RunWith
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.junit.JUnitRunner
+
+import scala.collection.mutable.ListBuffer
 
 
 /**
   * Created by margogo on 15.04.17.
   */
 @RunWith(classOf[JUnitRunner])
-class FieldSpec extends FlatSpec with Matchers {
-  "A Field" should "have all data" in {
-//    val testfield = Field (1, "Property","green", isUsed = false, Array(0,1,2),Array(10,11,12))
-//    testfield.id should be(1)
-//    testfield.property should be("Property")
-//    testfield.color should be("green")
-//    testfield.isUsed should be(false)
-//    testfield.predecessorIds should be(Array(0,1,2))
-//    testfield.successorIds should be(Array(10,11,12))
-  }
+class FieldSpec extends FlatSpec with MockFactory {
+
+  var figureList = new ListBuffer[Figure]
+  var playerList = new ListBuffer[Player]
+
+
+
 }
