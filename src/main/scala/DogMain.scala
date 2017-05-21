b0a6093a9cd18921723e4c39f2ee4406d9de0856
Markus Buechler <markus.buechler@mail.de>
2017-05-21 17:36:00 +0200

    final try using scalafxml

--- src/main/scala/DogMain.scala
+++ src/main/scala/DogMain.scala
@@ -4,34 +4,37 @@
   */
 
 import de.htwg.se.yourgame.DependencyModule
-import de.htwg.se.yourgame.view.{DogGui, DogTui}
 import com.google.inject.{Guice, Injector}
-import net.codingwell.scalaguice.InjectorExtensions._
+import java.io.IOException
 
-import scalafxml.core.{DependenciesByType, FXMLView}
-
+import de.htwg.se.yourgame.view.DogTui
 
 import scalafx.Includes._
 import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
+import scalafxml.core.{FXMLView, NoDependencyResolver}
+import net.codingwell.scalaguice.InjectorExtensions._
 
 
+object DogApplication extends JFXApp {
 
-  object DogApplication extends JFXApp {
+  val injector: Injector = Guice.createInjector(new DependencyModule)
 
+  val resource = getClass.getResource("/de/htwg/se/yourgame/fxml/main.fxml")
 
+  if (resource == null) {
+    throw new IOException("Cannot load resource: main.fxml")
+  }
 
-    val injector: Injector = Guice.createInjector(new DependencyModule)
+  val root = FXMLView(resource, NoDependencyResolver)
 
-    val root = FXMLView(getClass.getResource("/de/htwg/se/yourgame/fxml/main.fxml"),
-      new DependenciesByType(Map()))
+  stage = new PrimaryStage() {
+    title = "FXML GridPane Demo"
+    scene = new Scene(root)
+  }
 
-    stage = new JFXApp.PrimaryStage() {
-      title = "DOG - THE GAME"
-      scene = new Scene(root)
-    }
-
-    var tui = injector.instance[DogTui]
+  var tui = injector.instance[DogTui]
 }
 
 
